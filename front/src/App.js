af4fc79b115b153904132ad9f0810edd7460f3d2
Michel <descotesmichel@aol.com>
2018-12-11 16:32:51 +0100

    quete020

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import SignUp from "./SignUp";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "mon@email.com",
+      password: "monPassw0rd",
+      passwordbis: "passwordBis",
+      name: "James",
+      lastname: "Bond"
+    };
+    this.updateField = this.updateField.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  updateField(event) {
+    let inputName = event.target.name;
+    this.setState({
+      [inputName]: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="App">
-        <SignUp />
+        <SignUp
+          updateField={this.updateField}
+          state={this.state}
+          handleSubmit={this.handleSubmit}
+        />
       </div>
     );
   }
--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -1,27 +1,54 @@
 import React, { Component } from "react";
 
 class SignUp extends Component {
-  constructor() {
-    super();
-    this.state = {
-      email: ""
-    };
-    this.updateEmailField = this.updateEmailField.bind(this);
-  }
-  updateEmailField(event) {
-    this.setState({
-      email: event.target.value
-    });
-  }
   render() {
+    const { updateField, state, handleSubmit } = { ...this.props };
     return (
       <div>
-        <h1>{this.state.email}</h1>
-        <input
-          onChange={event => this.updateEmailField(event)}
-          type="email"
-          name="email"
-        />
+        <h1>{JSON.stringify(state, 1, 1)}</h1>
+        <form onSubmit={event => handleSubmit(event)}>
+          <p>
+            <input
+              onChange={event => updateField(event)}
+              type="email"
+              name="email"
+              placeholder="Votre email"
+            />
+          </p>
+          <p>
+            <input
+              onChange={event => updateField(event)}
+              type="password"
+              name="password"
+              placeholder="Votre mot de passe"
+            />
+          </p>
+          <p>
+            <input
+              onChange={event => updateField(event)}
+              type="password"
+              name="passwordbis"
+              placeholder="Retappez votre mot de passe"
+            />
+          </p>
+          <p>
+            <input
+              onChange={event => updateField(event)}
+              type="text"
+              name="name"
+              placeholder="Votre Nom"
+            />
+          </p>
+          <p>
+            <input
+              onChange={event => updateField(event)}
+              type="text"
+              name="firstname"
+              placeholder="Votre prénom"
+            />
+          </p>
+          <input type="submit" value="Soumettre" />
+        </form>
       </div>
     );
   }
