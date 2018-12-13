9b7dab365bea30efc2e79cbcd864094d0e59065a
Michel <descotesmichel@aol.com>
2018-12-13 18:15:30 +0100

    Quete040 modif

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const connection = require("../../helpers/db.js");
-router.post("/signup", function(req, res, next) {
+router.post("/signup", (req, res, next) => {
   const formData = req.body;
+  console.log(formData);
   connection.query("INSERT INTO users SET ?", formData, (err, results) => {
-    if (err) res.status(500).json({ flash: err.message });
-    else res.status(200).json({ flash: "User has been signed up !" });
+    if (err) {
+      res.status(500).json({ flash: err.message });
+    } else {
+      res.status(200).json({ flash: "User has been signed up !" });
+    }
   });
 });
 
