259c839b5f04b91db8094027de7921da024b4509
Michel <descotesmichel@aol.com>
2018-12-14 11:20:16 +0100

    Quete040 changement modif dans auth

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const connection = require("../../helpers/db.js");
-router.post("/signup", (req, res, next) => {
-  const formData = req.body;
-  console.log(formData);
-  connection.query("INSERT INTO users SET ?", formData, (err, results) => {
-    if (err) {
-      res.status(500).json({ flash: err.message });
-    } else {
+
+router.post("/signup", function(req, res, next) {
+  connection.query(
+    "INSERT INTO users (email, password, name, lastname) VALUES(?,?,?,?)",
+    [req.body.email, req.body.password, req.body.name, req.body.lastname],
+    function(error, results, fields) {
+      if (error) {
+        res.status(500).json({ flash: error.message });
+      }
       res.status(200).json({ flash: "User has been signed up !" });
     }
-  });
+  );
 });
 
 module.exports = router;
