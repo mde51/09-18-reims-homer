45e9c4e7ab88204db0ec34957b4ac9caefd7733e
Michel <descotesmichel@aol.com>
2018-12-24 11:03:17 +0100

    Quete060

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import Paper from "@material-ui/core/Paper";
 import "./App.css";
 
 import SignUp from "./SignUp";
@@ -6,9 +9,42 @@ import SignUp from "./SignUp";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <SignUp />
-      </div>
+      <MuiThemeProvider>
+        <Grid
+          container
+          alignItems="center"
+          justify="center"
+          style={{ height: "100%" }}
+        >
+          <Grid item xs={12} sm={6}>
+            <Paper elevation={6} style={{ margin: 32 }}>
+              <Grid
+                container
+                alignItems="center"
+                alignContent="center"
+                justify="center"
+              >
+                <Grid item xs={12} sm={6} style={{ textAlign: "center" }}>
+                  <img
+                    src="http://images.innoveduc.fr/react_odyssey_homer/wildhomer.png"
+                    alt="homer"
+                  />
+                </Grid>
+
+                <Grid
+                  container
+                  item
+                  xs={12}
+                  sm={6}
+                  justify="center"
+                  style={{ textAlign: "center" }}
+                />
+                <SignUp />
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </MuiThemeProvider>
     );
   }
 }
--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -1,90 +1,129 @@
 import React, { Component } from "react";
+import { TextField, Button, Snackbar } from "@material-ui/core";
 
 class SignUp extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      email: "",
-      password: "",
-      passwordbis: "",
-      name: "",
-      lastname: "",
+      email: "mon@email.com",
+      password: "monPassw0rd",
+      passwordbis: "monPassw0rd",
+      name: "James",
+      lastname: "Bond",
       flash: ""
     };
-    this.updateField = this.updateField.bind(this);
+    this.updateEmailField = this.updateEmailField.bind(this);
+    this.updatePassword = this.updatePassword.bind(this);
+    this.updatePasswordbis = this.updatePasswordbis.bind(this);
+    this.updateName = this.updateName.bind(this);
+    this.updateLastname = this.updateLastname.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  updateField = event => {
+  updateEmailField(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  updatePassword(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  updatePasswordbis(event) {
+    this.setState({ passwordbis: event.target.value });
+  }
+
+  updateName(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  updateLastname(event) {
+    this.setState({ lastname: event.target.value });
+  }
+
+  handleClick = () => {
     this.setState({
-      [event.target.name]: event.target.value
+      open: true
     });
   };
 
-  handleSubmit = () => {
+  handleRequestClose = () => {
+    this.setState({
+      open: false
+    });
+  };
+
+  handleSubmit(event) {
+    console.log("A name was submitted: " + JSON.stringify(this.state, 1, 1));
     fetch("/auth/signup", {
       method: "POST",
       headers: new Headers({
         "Content-Type": "application/json"
       }),
-      body: JSON.stringify(this.state)
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        name: this.state.name,
+        lastname: this.state.lastname
+      })
     })
       .then(res => res.json())
       .then(
-        res => this.setState({ flash: res.flash }),
-        err => this.setState({ flash: err.flash })
+        res => this.setState({ flash: res.flash, open: true }),
+        err => this.setState({ flash: err.flash, open: true })
       );
-  };
-
+    event.preventDefault();
+  }
   render() {
-    const myJSON = JSON.stringify(this.state);
     return (
-      <form onSubmit={this.handleSubmit}>
-        <br />
-        <br />
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>E-mail</h4>
+          <TextField
+            onChange={this.updateEmailField}
+            type="email"
+            name="email"
+          />
+          <h4>Password</h4>
+          <TextField
+            onChange={this.updatePassword}
+            type="password"
+            name="password"
+          />
+          <h4>Repeat password</h4>
+          <TextField
+            onChange={this.updatePasswordbis}
+            type="password"
+            name="passwordbis"
+          />
+          <h4>Name</h4>
+          <TextField onChange={this.updateName} type="text" name="name" />
+          <h4>Lastname</h4>
+          <TextField
+            onChange={this.updateLastname}
+            type="text"
+            name="lastname"
+          />
+          <br />
+          <br />
 
-        <input
-          onChange={this.updateField}
-          type="email"
-          name="email"
-          placeholder="enter your email"
-        />
-        <br />
-        <br />
-        <input
-          onChange={this.updateField}
-          type="password"
-          name="password"
-          placeholder="enter your password"
-        />
-        <br />
-        <br />
-        <input
-          onChange={this.updateField}
-          type="password"
-          name="passwordbis"
-          placeholder="enter your password again"
-        />
-        <br />
-        <br />
-        <input
-          onChange={this.updateField}
-          type="text"
-          name="name"
-          placeholder="enter your first name"
-        />
-        <br />
-        <br />
-        <input
-          onChange={this.updateField}
-          type="text"
-          name="lastname"
-          placeholder="enter your last name"
-        />
-        <br />
-        <br />
-        <input type="submit" value="Soumettre" />
-      </form>
+          <Button
+            color="primary"
+            variant="contained"
+            type="submit"
+            value="Soumettre"
+            onClick={this.handleClick}
+          >
+            Valider
+          </Button>
+
+          <Snackbar
+            open={this.state.open}
+            message="Vous êtes inscrit !!"
+            autoHideDuration={4000}
+            onRequestClose={this.handleRequestClose}
+          />
+        </form>
+      </div>
     );
   }
 }
