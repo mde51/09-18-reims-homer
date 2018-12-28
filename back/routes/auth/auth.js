2d468d0c1809dd186d0ca83465e71fed5aab941e
Michel <descotesmichel@aol.com>
2018-12-28 19:00:52 +0100

    Quete 080

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -6,17 +6,17 @@ const connection = require("../../helpers/db.js");
 const passport = require("passport");
 const bcrypt = require("bcrypt");
 
-router.post("/signup", function(req, res, next) {
+router.post("/signup", function(req, res) {
   const formData = req.body;
   const hash = bcrypt.hashSync(req.body.password, 10);
   formData.password = hash;
-  connection.query("INSERT INTO users SET ?", [formData], (err, results) => {
+  connection.query("INSERT INTO users SET ?", formData, err => {
     if (err) res.status(500).json({ flash: err.message });
     else res.status(200).json({ flash: "User has been signed up !" });
   });
 });
 
-router.post("/signin", function(req, res, next) {
+router.post("/signin", function(req, res) {
   passport.authenticate("local", (err, user, info) => {
     if (err) return res.status(500).send(err);
     if (!user) return res.status(400).json({ message: info.message });
