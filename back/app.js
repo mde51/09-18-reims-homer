0c1eeaa94c456c1519d20fc322b5dd381b1649f6
Michel <descotesmichel@aol.com>
2019-01-02 22:38:21 +0100

    Quete 080

--- back/app.js
+++ back/app.js
@@ -6,33 +6,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
-<<<<<<< HEAD
-<<<<<<< HEAD
-const authRouter = require("./routes/auth/auth.js");
-const passport = require("passport");
-const multer = require("multer");
-const fs = require("fs");
-
-const upload = multer({
-  dest: "tmp/",
-  fileFilter: function(req, file, cb) {
-    if (file.mimetype !== "image/png") {
-      return cb(null, false);
-    } else {
-      cb(null, true);
-    }
-  },
-  limits: {
-    fileSize: 3 * 1024 * 1024
-  }
-});
-=======
 const authRouter = require("./routes/auth/auth");
->>>>>>> 9877caac5994d22753c32c46ec72bbb757d0625c
-=======
-const authRouter = require("./routes/auth/auth.js");
+
 const passport = require("passport");
->>>>>>> parent of f099cb6... UPLoadFichier
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -44,13 +20,10 @@ app.get("/", (req, res) => {
 });
 
 app.use("/auth", authRouter);
-<<<<<<< HEAD
 
-<<<<<<< HEAD
 app.get("/profile", passport.authenticate("jwt", { session: false }), function(
-=======
+
 app.post("/uploaddufichier", upload.array("monfichier"), function(
->>>>>>> 9877caac5994d22753c32c46ec72bbb757d0625c
   req,
   res,
   next
@@ -67,9 +40,8 @@ app.post("/uploaddufichier", upload.array("monfichier"), function(
 });
 
 app.post("/uploaddufichier", upload.array("monfichier"), function(
-=======
+
 app.get("/profile", passport.authenticate("jwt", { session: false }), function(
->>>>>>> parent of f099cb6... UPLoadFichier
   req,
   res
 ) {
